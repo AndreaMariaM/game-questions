047e39e0d982d989c4147e94738daef4552c33e2
Andrea <amatellian@kythera.space>
2021-02-24 16:53:09 -0500

    fix to the broken map and indentation

--- src/gameService/index.js
+++ src/gameService/index.js
@@ -1,374 +1,299 @@
 const qBankSFW = [
-    {
-        q:
-            "If you could choose any food to eat for free whenever you want, what would you choose?",
-        Id: "1"
-    },
-    {
-        q: 
-            "How do you think your best friend would describe you in one word?",
-        Id: "2"
-    },
-    {
-        q:
-            "If you could travel to any country today, which would you choose?",
-        Id: "3"
-    },
-    {
-        q: 
-            "What is the wildest thing you've ever done?",
-        Id: "4"
-    },
-    {
-        q:
-            "What is the biggest thing you'd really hate to be known for?",
-        Id: "5"
-    },
-    {
-        q:
-            "What would be fun to do if it weren't a crime?",
-        Id: "6"
-    },
-    {
-        q:
-            "What is something you often crave?",
-        Id: "7"
-    },
-    {
-        q:
-            "What's a cheezy thing to own?",
-        Id: "8"
-    },
-    {
-        q:
-            "What is something you refuse to share?",
-        Id: "9"
-    },
-    {
-        q:
-            "If you were in the news what would it be for?",
-        Id: "10"
-    },
-    {
-        q:
-            "What's your best dad joke?",
-        Id: "11"
-    },
-    {
-        q:
-            "What do you think is the best song for a first dance at a wedding?",
-        Id: "12"
-    },
-    {
-        q:
-            "If you became a millionare what would you buy?",
-        Id: "13"
-    },
-    {
-        q:
-            "What can you never imagine owning?",
-        Id: "14"
-    },
-    {
-        q:
-            "What email addres would you use to spam someone you dislike?",
-        Id: "15"
-    },
-    {
-        q:
-            "What's a tv show or movie you'd reccomend to me?",
-        Id: "16"
-    },
-    {
-        q:
-            "What lie would you tell to impress someone?",
-        Id: "17"
-    },
-    {
-        q:
-            "What's the most dangerous thing you've done?",
-        Id: "18"
-    },
-    {
-        q:
-            "What do you first notice when tou meet someone?",
-        Id: "19"
-    },
-    {
-        q:
-            "Whats the weirdest place to take someone on a date?",
-        Id: "20"
-    },
-    {
-        q:
-            "Whats the weirdest favor you think you could get someone to do?",
-        Id: "21"
-    },
-    {
-        q:
-            "What is cringey to you that most people enjoy?",
-        Id: "22"
-    },
-    {
-        q:
-            "What's the perfect wifi name?",
-        Id: "23"
-    },
-    {
-        q:
-            "What weird thing could you put in a pinata?",
-        Id: "24"
-    },
-    {
-        q:
-            "What's a sure sign someone's a physcopath?",
-        Id: "25"
-    },
-    {
-        q:
-            "What's the worst thing you've done drunk?",
-        Id: "26"
-    },
-    {
-        q:
-            "What sidekick would you choose to be friends with?",
-        Id: "27"
-    },
-    {
-        q:
-            "What's a state in the US you want to travel to?",
-        Id: "28"
-    },
-    {
-        q:
-            "What could someone say to entice you into a cult?",
-        Id: "29"
-    },
-    {
-        q:
-            "You have to eat one thing for the rest of your life what would it be?",
-        Id: "30"
-    },
-    {
-        q:
-            "If you had to get a wild animal as a pet what would you choose?",
-        Id: "31"
-    },
-    {
-        q:
-            "Choose one uncommon weird haircut you wouldn't get",
-        Id: "32"
-    },
-    {
-        q:
-            "What is your favorite kind of beer?",
-        Id: "33"
-    },
-    {
-        q:
-            "Which chacacter from a show or movie do you relate to?",
-        Id: "34"
-    },
-    {
-        q:
-            "If you could get paid to do anything what would you choose?",
-        Id: "35"
-    }
-
+  {
+    q:
+      "If you could choose any food to eat for free whenever you want, what would you choose?",
+    Id: "1",
+  },
+  {
+    q: "How do you think your best friend would describe you in one word?",
+    Id: "2",
+  },
+  {
+    q: "If you could travel to any country today, which would you choose?",
+    Id: "3",
+  },
+  {
+    q: "What is the wildest thing you've ever done?",
+    Id: "4",
+  },
+  {
+    q: "What is the biggest thing you'd really hate to be known for?",
+    Id: "5",
+  },
+  {
+    q: "What would be fun to do if it weren't a crime?",
+    Id: "6",
+  },
+  {
+    q: "What is something you often crave?",
+    Id: "7",
+  },
+  {
+    q: "What's a cheezy thing to own?",
+    Id: "8",
+  },
+  {
+    q: "What is something you refuse to share?",
+    Id: "9",
+  },
+  {
+    q: "If you were in the news what would it be for?",
+    Id: "10",
+  },
+  {
+    q: "What's your best dad joke?",
+    Id: "11",
+  },
+  {
+    q: "What do you think is the best song for a first dance at a wedding?",
+    Id: "12",
+  },
+  {
+    q: "If you became a millionare what would you buy?",
+    Id: "13",
+  },
+  {
+    q: "What can you never imagine owning?",
+    Id: "14",
+  },
+  {
+    q: "What email addres would you use to spam someone you dislike?",
+    Id: "15",
+  },
+  {
+    q: "What's a tv show or movie you'd reccomend to me?",
+    Id: "16",
+  },
+  {
+    q: "What lie would you tell to impress someone?",
+    Id: "17",
+  },
+  {
+    q: "What's the most dangerous thing you've done?",
+    Id: "18",
+  },
+  {
+    q: "What do you first notice when tou meet someone?",
+    Id: "19",
+  },
+  {
+    q: "Whats the weirdest place to take someone on a date?",
+    Id: "20",
+  },
+  {
+    q: "Whats the weirdest favor you think you could get someone to do?",
+    Id: "21",
+  },
+  {
+    q: "What is cringey to you that most people enjoy?",
+    Id: "22",
+  },
+  {
+    q: "What's the perfect wifi name?",
+    Id: "23",
+  },
+  {
+    q: "What weird thing could you put in a pinata?",
+    Id: "24",
+  },
+  {
+    q: "What's a sure sign someone's a physcopath?",
+    Id: "25",
+  },
+  {
+    q: "What's the worst thing you've done drunk?",
+    Id: "26",
+  },
+  {
+    q: "What sidekick would you choose to be friends with?",
+    Id: "27",
+  },
+  {
+    q: "What's a state in the US you want to travel to?",
+    Id: "28",
+  },
+  {
+    q: "What could someone say to entice you into a cult?",
+    Id: "29",
+  },
+  {
+    q: "You have to eat one thing for the rest of your life what would it be?",
+    Id: "30",
+  },
+  {
+    q: "If you had to get a wild animal as a pet what would you choose?",
+    Id: "31",
+  },
+  {
+    q: "Choose one uncommon weird haircut you wouldn't get",
+    Id: "32",
+  },
+  {
+    q: "What is your favorite kind of beer?",
+    Id: "33",
+  },
+  {
+    q: "Which chacacter from a show or movie do you relate to?",
+    Id: "34",
+  },
+  {
+    q: "If you could get paid to do anything what would you choose?",
+    Id: "35",
+  },
 ];
 const qBankNSFW = [
-    {
-        q: 
-            "What should everyone expect from a sexual partner?",
-        Id: "1"
-    },
-    {
-        q:
-            "What is the sexiest thing someone could wear?",
-        Id: "2"
-    },
-    {
-        q:
-            "Who would you be happy to know has a nude photo of you?",
-        Id: "3"
-    },
-    {
-        q:
-            "What song always puts you kn the mood?",
-        Id: "4"
-    },
-    {
-        q:
-            "What do you think is the most common fantasy?",
-        Id: "5"
-    },
-    {
-        q:
-            "If you were arrested what would it be for?",
-        Id: "6"
-    },
-    {
-        q:
-            "What would be a good name for a new sex toy?",
-        Id: "7"
-    },
-    {
-        q:
-            "If you developed a lingerie line what would you call it?",
-        Id: "8"
-    },
-    {
-        q:
-            "What's a fetish you wouldn't partake in for 1 million dollars?",
-        Id: "9"
-    },
-    {
-        q:
-            "Come up with a name for a new sex position.",
-        Id: "10"
-    },
-    {
-        q:
-            "Who is someone you would do anything to kiss?",
-        Id: "11"
-    },
-    {
-        q:
-            "What could a partner do to  make you q the relationship?",
-        Id: "12"
-    },
-    {
-        q:
-            "What's your biggest turn off?",
-        Id: "13"
-    },
-    {
-        q:
-            "What's your best pickup line?",
-        Id: "14"
-    },
-    {
-        q:
-            "Whats the most romantic thing you've done",
-        Id: "15"
-    },
-    {
-        q:
-            "What's a drug you'd never do?",
-        Id: "16"
-    },
-    {
-        q:
-            "What category of porn freaks you out/disgusts you?",
-        Id: "17"
-    },
-    {
-        q:
-            "If you gave a sexy massage what would be the grand finale?",
-        Id: "18"
-    },
-    {
-        q:
-            "What's a fetish you'd try?",
-        Id: "19"
-    },
-    {
-        q:
-            "What category should be added to beauty pagents?",
-        Id: "20"
-    },
-    {
-        q:
-            "What's one serial killer trait?",
-        Id: "21"
-    },
-    {
-        q:
-            "What's the most risky place you've had sex?",
-        Id: "22"
-    },
-    {
-        q:
-            "What's an inappropriate place to breakup with someone?",
-        Id: "23"
-    },
-    {
-        q:
-            "What celeb would you want to have phone sex with?",
-        Id: "24"
-    },
-    {
-        q:
-            "What would get you to fight someone?",
-        Id: "25"
-    },
-    {
-        q:
-            "Make up a new slang word for dick.",
-        Id: "26"
-    },
-    {
-        q:
-            "What's something that would cause you to leave a first date?",
-        Id: "27"
-    },
-    {
-        q:
-            "What tv show do you pick for masturbation background?",
-        Id: "28"
-    },
-    {
-        q:
-            "Who would make the worst wingman?",
-        Id: "29"
-    },
-    {
-        q:
-            "What word sounds dirty with a british accent?",
-        Id: "30"
-    },
-    {
-        q:
-            "In what public place would you want to have sex?",
-        Id: "31"
-    },
-    {
-        q:
-            "What would you be willing to do to get out of a speeding ticket?",
-        Id: "32"
-    },
-    {
-        q:
-            "What would you attempt to steal if someone dared you?",
-        Id: "33"
-    },
-    {
-        q:
-            "What's something illegal you've done?",
-        Id: "34"
-    },
-    {
-        q:
-            "Where are you planning your best friend's bachelor/ette party?",
-        Id: "35"
-    },
-    {
-        q:
-            "How are you convincing your partner to have a threesome?",
-        Id: "36"
-    },
-    {
-        q:
-            "What's your ideal roleplay?",
-        Id: "37"
-    },
-    {
-        q:
-            "Where are you disposing of the person you just murdered?",
-        Id: "38"
-    }
+  {
+    q: "What should everyone expect from a sexual partner?",
+    Id: "1",
+  },
+  {
+    q: "What is the sexiest thing someone could wear?",
+    Id: "2",
+  },
+  {
+    q: "Who would you be happy to know has a nude photo of you?",
+    Id: "3",
+  },
+  {
+    q: "What song always puts you kn the mood?",
+    Id: "4",
+  },
+  {
+    q: "What do you think is the most common fantasy?",
+    Id: "5",
+  },
+  {
+    q: "If you were arrested what would it be for?",
+    Id: "6",
+  },
+  {
+    q: "What would be a good name for a new sex toy?",
+    Id: "7",
+  },
+  {
+    q: "If you developed a lingerie line what would you call it?",
+    Id: "8",
+  },
+  {
+    q: "What's a fetish you wouldn't partake in for 1 million dollars?",
+    Id: "9",
+  },
+  {
+    q: "Come up with a name for a new sex position.",
+    Id: "10",
+  },
+  {
+    q: "Who is someone you would do anything to kiss?",
+    Id: "11",
+  },
+  {
+    q: "What could a partner do to  make you q the relationship?",
+    Id: "12",
+  },
+  {
+    q: "What's your biggest turn off?",
+    Id: "13",
+  },
+  {
+    q: "What's your best pickup line?",
+    Id: "14",
+  },
+  {
+    q: "Whats the most romantic thing you've done",
+    Id: "15",
+  },
+  {
+    q: "What's a drug you'd never do?",
+    Id: "16",
+  },
+  {
+    q: "What category of porn freaks you out/disgusts you?",
+    Id: "17",
+  },
+  {
+    q: "If you gave a sexy massage what would be the grand finale?",
+    Id: "18",
+  },
+  {
+    q: "What's a fetish you'd try?",
+    Id: "19",
+  },
+  {
+    q: "What category should be added to beauty pagents?",
+    Id: "20",
+  },
+  {
+    q: "What's one serial killer trait?",
+    Id: "21",
+  },
+  {
+    q: "What's the most risky place you've had sex?",
+    Id: "22",
+  },
+  {
+    q: "What's an inappropriate place to breakup with someone?",
+    Id: "23",
+  },
+  {
+    q: "What celeb would you want to have phone sex with?",
+    Id: "24",
+  },
+  {
+    q: "What would get you to fight someone?",
+    Id: "25",
+  },
+  {
+    q: "Make up a new slang word for dick.",
+    Id: "26",
+  },
+  {
+    q: "What's something that would cause you to leave a first date?",
+    Id: "27",
+  },
+  {
+    q: "What tv show do you pick for masturbation background?",
+    Id: "28",
+  },
+  {
+    q: "Who would make the worst wingman?",
+    Id: "29",
+  },
+  {
+    q: "What word sounds dirty with a british accent?",
+    Id: "30",
+  },
+  {
+    q: "In what public place would you want to have sex?",
+    Id: "31",
+  },
+  {
+    q: "What would you be willing to do to get out of a speeding ticket?",
+    Id: "32",
+  },
+  {
+    q: "What would you attempt to steal if someone dared you?",
+    Id: "33",
+  },
+  {
+    q: "What's something illegal you've done?",
+    Id: "34",
+  },
+  {
+    q: "Where are you planning your best friend's bachelor/ette party?",
+    Id: "35",
+  },
+  {
+    q: "How are you convincing your partner to have a threesome?",
+    Id: "36",
+  },
+  {
+    q: "What's your ideal roleplay?",
+    Id: "37",
+  },
+  {
+    q: "Where are you disposing of the person you just murdered?",
+    Id: "38",
+  },
 ];
 export default (n = 3) =>
-    Promise.resolve(qBankSFW.sort(() => 0.5 - Math.random()).slice(0,n));
-
-    
+  Promise.resolve(qBankSFW.sort(() => 0.5 - Math.random()).slice(0, n));
--- src/index.js
+++ src/index.js
@@ -2,62 +2,67 @@ import React, { Component } from "react";
 import ReactDOM from "react-dom";
 import "./assets/style.css";
 import gameService from "./gameService";
-import QuestionBox from "./components/QuestionBox";
 
-class Game extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            questionBank: [],
-            selected: ""
-        };
-        this.Greet = this.Greet.bind(this);
-    }
-    getQuestions = () => {
-        gameService().then(question => {
-            this.setState({
-                questionBank: question
-            });
-        });
+export class Questions extends Component {
+  // we will have a unmount and mount phase and a call within the greater game.
+  constructor(props) {
+    super(props);
+    this.state = {
+      questionBank: [],
+      selected: "",
     };
-    componentDidMount() {
-        this.getQuestions();
-    };
-    Greet() {
-        if (this.state.selected === "") {
-            return( 
-                <>
-                    <div className="title">Choose a Question</div>
-                    {this.state.questionBank.length > 0 && this.state.questionBank.map(({q, Id}) => {
-                        <div className="QuestionBox">
-                            <QuestionBox question={q}/>
-                            {console.log(q)}
-                            <button className="answerBtn" onClick={() => {
-                                this.setState({
-                                    selected: {q}
-                                });
-                            }}>Select</button>
-                        </div>
-                    })}
-                </>
-            );
-        }
-        return (
-            <div>
-                <div className="title">Answer the Question</div>
-                <div className="questionBox">
-                    <div className="question">{this.state.question}</div>
-                </div>
-            </div>
-        );
+    this.Greet = this.Greet.bind(this);
+  }
+  getQuestions = () => {
+    gameService().then((question) => {
+      this.setState({
+        questionBank: question,
+      });
+    });
+  };
+  componentDidMount() {
+    this.getQuestions();
+  }
+  Greet() {
+    if (this.state.selected === "") {
+      return (
+        <>
+          <div className="title">Choose a Question</div>
+          {this.state.questionBank.length > 0 &&
+            this.state.questionBank.map(({ q, Id }) => (
+              <div className="QuestionBox">
+                <div className="question">{q}</div>
+                <button
+                  className="answerBtn"
+                  onClick={() => {
+                    this.setState({
+                      selected: { q },
+                    });
+                  }}
+                >
+                  Select
+                </button>
+              </div>
+            ))}
+        </>
+      );
     }
-    render() {
-        return (
-            <div className="container">
-                < this.Greet />
-            </div>
-        );
-    }
+    return (
+      <div>
+        <div className="title">Answer the Question</div>
+        <div className="QuestionBox">
+          <div className="question">{this.state.question}</div>
+        </div>
+      </div>
+    );
+  }
+  render() {
+    return (
+      <div className="container">
+        <this.Greet />
+      </div>
+    );
+  }
 }
 
-ReactDOM.render(<Game />, document.getElementById("root"));
+ReactDOM.render(<Questions />, document.getElementById("root"));
