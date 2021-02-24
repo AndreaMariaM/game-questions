2c5ff8770bd3d46763d88396b049991a6914f9b0
Andrea <amatellian@kythera.space>
2021-02-23 21:59:35 -0500

    Add more Q's and remove old default React stuff

--- src/gameService/index.js
+++ src/gameService/index.js
@@ -23,11 +23,352 @@ const qBankSFW = [
         question:
             "What is the biggest thing you'd really hate to be known for?",
         Id: "5"
+    },
+    {
+        question:
+            "What would be fun to do if it weren't a crime?",
+        Id: "6"
+    },
+    {
+        question:
+            "What is something you often crave?",
+        Id: "7"
+    },
+    {
+        question:
+            "What's a cheezy thing to own?",
+        Id: "8"
+    },
+    {
+        question:
+            "What is something you refuse to share?",
+        Id: "9"
+    },
+    {
+        question:
+            "If you were in the news what would it be for?",
+        Id: "10"
+    },
+    {
+        question:
+            "What's your best dad joke?",
+        Id: "11"
+    },
+    {
+        question:
+            "What do you think is the best song for a first dance at a wedding?",
+        Id: "12"
+    },
+    {
+        question:
+            "If you became a millionare what would you buy?",
+        Id: "13"
+    },
+    {
+        question:
+            "What can you never imagine owning?",
+        Id: "14"
+    },
+    {
+        question:
+            "What email addres would you use to spam someone you dislike?",
+        Id: "15"
+    },
+    {
+        question:
+            "What's a tv show or movie you'd reccomend to me?",
+        Id: "16"
+    },
+    {
+        question:
+            "What lie would you tell to impress someone?",
+        Id: "17"
+    },
+    {
+        question:
+            "What's the most dangerous thing you've done?",
+        Id: "18"
+    },
+    {
+        question:
+            "What do you first notice when tou meet someone?",
+        Id: "19"
+    },
+    {
+        question:
+            "Whats the weirdest place to take someone on a date?",
+        Id: "20"
+    },
+    {
+        question:
+            "Whats the weirdest favor you think you could get someone to do?",
+        Id: "21"
+    },
+    {
+        question:
+            "What is cringey to you that most people enjoy?",
+        Id: "22"
+    },
+    {
+        question:
+            "What's the perfect wifi name?",
+        Id: "23"
+    },
+    {
+        question:
+            "What weird thing could you put in a pinata?",
+        Id: "24"
+    },
+    {
+        question:
+            "What's a sure sign someone's a physcopath?",
+        Id: "25"
+    },
+    {
+        question:
+            "What's the worst thing you've done drunk?",
+        Id: "26"
+    },
+    {
+        question:
+            "What sidekick would you choose to be friends with?",
+        Id: "27"
+    },
+    {
+        question:
+            "What's a state in the US you want to travel to?",
+        Id: "28"
+    },
+    {
+        question:
+            "What could someone say to entice you into a cult?",
+        Id: "29"
+    },
+    {
+        question:
+            "You have to eat one thing for the rest of your life what would it be?",
+        Id: "30"
+    },
+    {
+        question:
+            "If you had to get a wild animal as a pet what would you choose?",
+        Id: "31"
+    },
+    {
+        question:
+            "Choose one uncommon weird haircut you wouldn't get",
+        Id: "32"
+    },
+    {
+        question:
+            "What is your favorite kind of beer?",
+        Id: "33"
+    },
+    {
+        question:
+            "Which chacacter from a show or movie do you relate with?",
+        Id: "34"
+    },
+    {
+        question:
+            "If you could get paid to do anything what would you choose?",
+        Id: "35"
     }
 
 ];
 const qBankNSFW = [
-
+    {
+        question: 
+            "What should everyone expect from a sexual partner?",
+        Id: "1"
+    },
+    {
+        question:
+            "What is the sexiest thing someone could wear?",
+        Id: "2"
+    },
+    {
+        question:
+            "Who would you be happy to know has a nude photo of you?",
+        Id: "3"
+    },
+    {
+        question:
+            "What song always puts you kn the mood?",
+        Id: "4"
+    },
+    {
+        question:
+            "What do you think is the most common fantasy?",
+        Id: "5"
+    },
+    {
+        question:
+            "If you were arrested what would it be for?",
+        Id: "6"
+    },
+    {
+        question:
+            "What would be a good name for a new sex toy?",
+        Id: "7"
+    },
+    {
+        question:
+            "If you developed a lingerie line what would you call it?",
+        Id: "8"
+    },
+    {
+        question:
+            "What's a fetish you wouldn't partake in for 1 million dollars?",
+        Id: "9"
+    },
+    {
+        question:
+            "Come up with a name for a new sex position.",
+        Id: "10"
+    },
+    {
+        question:
+            "Who is someone you would do anything to kiss?",
+        Id: "11"
+    },
+    {
+        question:
+            "What could a partner do to  make you question the relationship?",
+        Id: "12"
+    },
+    {
+        question:
+            "What's your biggest turn off?",
+        Id: "13"
+    },
+    {
+        question:
+            "What's your best pickup line?",
+        Id: "14"
+    },
+    {
+        question:
+            "Whats the most romantic thing you've done",
+        Id: "15"
+    },
+    {
+        question:
+            "What's a drug you'd never do?",
+        Id: "16"
+    },
+    {
+        question:
+            "What category of porn freaks you out/disgusts you?",
+        Id: "17"
+    },
+    {
+        question:
+            "If you gave a sexy massage what would be the grand finale?",
+        Id: "18"
+    },
+    {
+        question:
+            "What's a fetish you'd try?",
+        Id: "19"
+    },
+    {
+        question:
+            "What category should be added to beauty pagents?",
+        Id: "20"
+    },
+    {
+        question:
+            "What's one serial killer trait?",
+        Id: "21"
+    },
+    {
+        question:
+            "What's the most risky place you've had sex?",
+        Id: "22"
+    },
+    {
+        question:
+            "What's an inappropriate place to breakup with someone?",
+        Id: "23"
+    },
+    {
+        question:
+            "What celeb would you want to have phone sex with?",
+        Id: "24"
+    },
+    {
+        question:
+            "What would get you to fight someone?",
+        Id: "25"
+    },
+    {
+        question:
+            "Make up a new slang word for dick.",
+        Id: "26"
+    },
+    {
+        question:
+            "What's something that would cause you to leave a first date?",
+        Id: "27"
+    },
+    {
+        question:
+            "What tv show do you pick for masturbation background?",
+        Id: "28"
+    },
+    {
+        question:
+            "Who would make the worst wingman?",
+        Id: "29"
+    },
+    {
+        question:
+            "What word sounds dirty with a british accent?",
+        Id: "30"
+    },
+    {
+        question:
+            "In what public place would you want to have sex?",
+        Id: "31"
+    },
+    {
+        question:
+            "What would you be willing to do to get out of a speeding ticket?",
+        Id: "32"
+    },
+    {
+        question:
+            "What would you attempt to steal if someone dared you?",
+        Id: "33"
+    },
+    {
+        question:
+            "What's something illegal you've done?",
+        Id: "34"
+    },
+    {
+        question:
+            "Where are you planning your best friend's bachelor/ette party?",
+        Id: "35"
+    },
+    {
+        question:
+            "How are you convincing your partner to have a threesome?",
+        Id: "36"
+    },
+    {
+        question:
+            "What's your ideal roleplay?",
+        Id: "37"
+    },
+    {
+        question:
+            "Where are you disposing of the person you just murdered?",
+        Id: "38"
+    }
 ];
 export default (n = 3) =>
     Promise.resolve(qBankSFW.sort(() => 0.5 - Math.random()).slice(0,n));
+
+    
